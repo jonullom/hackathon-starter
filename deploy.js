54263dd69e71d83ddcd3467e4db93fa3e143854d
jonullom <37455554+jonullom@users.noreply.github.com>
2019-06-30 20:01:05 -0500

    fix issue with deploy.js

--- deploy.js
+++ deploy.js
@@ -84,7 +84,7 @@ function sshConnect() {
     ssh
         .connect({
             // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-            host: '18.212.51.15',
+            host: '3.83.81.92',
             username: 'ubuntu',
             privateKey: 'hs-key.pem'
         })
